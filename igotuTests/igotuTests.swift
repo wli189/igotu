3b0074b1fa9c9148337edb476cdc2230d9d6a416
brian <brianleelwt@gmail.com>
2026-08-17 17:41:46 -0500

    Initial Commit

---
+++ igotuTests/igotuTests.swift
@@ -0,0 +1,19 @@
+//
+//  igotuTests.swift
+//  igotuTests
+//
+//  Created by Brian Li on 8/17/26.
+//
+
+import Testing
+@testable import igotu
+
+struct igotuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ igotuUITests/igotuUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  igotuUITestsLaunchTests.swift
+//  igotuUITests
+//
+//  Created by Brian Li on 8/17/26.
+//
+
+import XCTest
+
+final class igotuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
